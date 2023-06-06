6184e11d852dbdf7f1d113fac2204c71bddf38dd
João Victor Dias de Moraes <joaovictorddm@outlook.com>
2023-06-06 09:40:14 -0300

    Parâmetros opcionais e rotas.

--- Pages/Categories.cshtml.cs
+++ Pages/Categories.cshtml.cs
@@ -7,16 +7,20 @@ namespace razor_app.Pages
     {
         public List<Category> Categories { get; set; } = new();
 
-        public void OnGet()
+        public void OnGet(int skip = 0, int take = 25)
         {
-            for (var i = 0; i <= 30; i++)
+            var temp = new List<Category>();
+
+            for (var i = 0; i <= 1500; i++)
             {
-                Categories.Add(
+                temp.Add(
                     new Category(i,
                     $"Categoria {i}",
                     i * 13.95M
                     ));
             }
+
+            Categories = temp.Skip(skip).Take(take).ToList();
         }
 
         public record Category(int Id, string Title, decimal Price);
